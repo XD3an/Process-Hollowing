b585e5e709319210b860486c59ac2630b1da13a2
XD3an <deanhs0dean@gmail.com>
2023-06-04 03:36:58 -0400

    update sec

--- setup.bat
+++ setup.bat
@@ -1,3 +1,5 @@
 cd src
 g++ ProcessHollowing.cpp -o ProcessHollowing.exe
-g++ target.cpp -o target.exe
+g++ local.cpp -o local.exe
+g++ remote.cpp -o remote.exe
+.\ProcessHollowing.exe
--- src/target.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int main() {
-    MessageBoxA(NULL, "Hello World!", "info", NULL);
-    return 0;
-}
